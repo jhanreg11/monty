92854328b86498207ea0e733e7cb1e960b9eed31
jacobhanson <jhanreg11@gmail.com>
2020-05-30 01:41:57 -0700

    added pretrained option

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import argparse
 from pipeline.model import Model
 from pipeline.preprocessor import PreProcessor
 from pipeline.tokenizer import PyTokenizer
+from tensorflow.keras.models import load_model
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train LSTM model')
@@ -11,6 +12,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=64, help='mini batch size')
     parser.add_argument('--buffer_dir', type=str, default='data/buffer')
     parser.add_argument('--clean_path', type=str, default='data/clean.py', help='path to file containing cleansed data')
+    parser.add_argument('--pretrained_path', type=str, default='')
     args = parser.parse_args()
 
     t = PyTokenizer(args.max_vocab_len)
@@ -18,4 +20,7 @@ if __name__ == '__main__':
     x, y = p.get_training_data(args.sample_len)
 
     m = Model(t.real_vocab_len, args.sample_len)
+    if args.pretrained_path:
+        m.model = load_model(args.pretrained_path)
+
     m.train(x, y, args.epochs, args.batch_size)
